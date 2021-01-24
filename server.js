f92fef076c747aeca201502380245c40b5795cba
Matthew Courtney <matthewdc182@gmail.com>
2021-01-23 22:21:45 -0500

    wrote get route to render notes/html

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var PORT = process.env.PORT || 5500;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
+app.get("/notes", function(req,res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
 app.get("*", function(req,res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
