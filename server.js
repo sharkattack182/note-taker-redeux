aabe94887addcdaff35085b52b3eb731ad357818
Matthew Courtney <matthewdc182@gmail.com>
2021-01-23 22:51:01 -0500

    got the api/notes rendering with a little work. the file structure is new so its a little tricky to get all my routes exact the first time.

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
-var path = require("path")
+var path = require("path");
+var fs = require("fs");
+
+var notesArray = [];
 
 var app = express();
 var PORT = process.env.PORT || 5500;
@@ -8,13 +11,28 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get("/notes", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
-})
-app.get("*", function(req,res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+// renders the two basic html pages
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.listen(PORT, function() {
-    console.log("Now Listening on PORT "+ PORT)
-})
+
+// api routes
+app.get("/api/notes", function (req, res) {
+
+    try {
+        notesArray = fs.readFileSync("db/db.json", "utf8");
+        notesArray = JSON.parse(notesArray);
+    } catch(err) {
+        console.log("\n error (in app.get.catch:");
+        console.log(err);
+    }
+    res.json(notesArray)
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.listen(PORT, function () {
+  console.log("Now Listening on PORT " + PORT);
+});
